206d9a1dc28f7d4a4d3399bab8b55bae69220262
Natalia Myshkina <natycik9494@mail.ru>
2020-10-07 18:55:51 -0400

    homework4.2 вопрос

--- projects/test/test1/index.js
+++ projects/test/test1/index.js
@@ -1,112 +1,80 @@
-import { loadAndSortTowns } from './functions';
 /*
- Страница должна предварительно загрузить список городов из
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- и отсортировать в алфавитном порядке.
+ ДЗ 7 - Создать редактор cookie с возможностью фильтрации
 
- При вводе в текстовое поле, под ним должен появляться список тех городов,
- в названии которых, хотя бы частично, есть введенное значение.
- Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+ 7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
+   - имя
+   - значение
+   - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
 
- Во время загрузки городов, на странице должна быть надпись "Загрузка..."
- После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+ 7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
+   - имя
+   - значение
+   - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
 
- Разметку смотрите в файле towns.html
+ Если добавляется cookie с именем уже существующей cookie, то ее значение в браузере и таблице должно быть обновлено
+
+ 7.3: На странице должно быть текстовое поле для фильтрации cookie
+ В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
+ Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ Если добавляемая cookie не соответствует фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляется cookie, с именем уже существующей cookie и ее новое значение не соответствует фильтру,
+ то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
 
  Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
-
- *** Часть со звездочкой ***
- Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
- то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
- При клике на кнопку, процесс загрузки повторяется заново
  */
 
+import './cookie.html';
+
 /*
- homeworkContainer - это контейнер для всех ваших домашних заданий
- Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+ app - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
 
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
  */
-
-import './towns.html';
-
 const homeworkContainer = document.querySelector('#homework-container');
+// текстовое поле для фильтрации cookie
+const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+// текстовое поле с именем cookie
+const addNameInput = homeworkContainer.querySelector('#add-name-input');
+// текстовое поле со значением cookie
+const addValueInput = homeworkContainer.querySelector('#add-value-input');
+// кнопка "добавить cookie"
+const addButton = homeworkContainer.querySelector('#add-button');
+// таблица со списком cookie
+const listTable = homeworkContainer.querySelector('#list-table tbody');
+//кнопка удалить
+const button = homeworkContainer.querySelector('#delete-button');
 
-/*
- Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
-
- Массив городов пожно получить отправив асинхронный запрос по адресу
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- */
-function loadTowns() {
-  return loadAndSortTowns();
-}
-
-/*
- Функция должна проверять встречается ли подстрока chunk в строке full
- Проверка должна происходить без учета регистра символов
-
- Пример:
-   isMatching('Moscow', 'moscow') // true
-   isMatching('Moscow', 'mosc') // true
-   isMatching('Moscow', 'cow') // true
-   isMatching('Moscow', 'SCO') // true
-   isMatching('Moscow', 'Moscov') // false
- */
-function isMatching(full, chunk) {
-  return full.toUpperCase().includes(chunk.toUpperCase());
-}
-
-/* Блок с надписью "Загрузка" */
-const loadingBlock = homeworkContainer.querySelector('#loading-block');
-/* Блок с надписью "Не удалось загрузить города" и кнопкой "Повторить" */
-const loadingFailedBlock = homeworkContainer.querySelector('#loading-failed');
-/* Кнопка "Повторить" */
-const retryButton = homeworkContainer.querySelector('#retry-button');
-/* Блок с текстовым полем и результатом поиска */
-const filterBlock = homeworkContainer.querySelector('#filter-block');
-/* Текстовое поле для поиска по городам */
-const filterInput = homeworkContainer.querySelector('#filter-input');
-/* Блок с результатами поиска */
-const filterResult = homeworkContainer.querySelector('#filter-result');
-
-retryButton.addEventListener('click', () => {
-  retry();
-});
-
-filterInput.addEventListener('input', function () {
-  updateFilter(this.value);
-});
-
-loadingFailedBlock.classList.add('hidden');
-filterBlock.classList.add('hidden');
-let towns = [];
-
-async function retry() {
-  try {
-    towns = await loadTowns();
-    loadingBlock.classList.add('hidden');
-    loadingFailedBlock.classList.add('hidden');
-    filterBlock.classList.remove('hidden');
-  } catch (e) {
-    loadingBlock.classList.add('hidden');
-    loadingFailedBlock.classList.remove('hidden');
-  }
-}
-
-function updateFilter(filterValue) {
-  filterResult.innerHTML = '';
-  const frag = document.createDocumentFragment();
-  for (const town of towns) {
-    if (filterValue && isMatching(town.name, filterValue)) {
-      const townD = document.createElement('div');
-      townD.textContent = town.name;
-      frag.append(townD);
+const filter = filterNameInput.value;
+let name = addNameInput.value;
+let value = addValueInput.value;
+filterNameInput.addEventListener('input', function () {
+  if (filter) {
+    if (filter.isMatch(name)) {
+      listTable.deleteRow(name);
     }
   }
-  filterResult.append(frag);
-}
-retry();
-export { loadTowns, isMatching };
+});
+
+addButton.addEventListener('click', () => {
+  homeworkContainer.cookie = `${name}=${value}`;
+  name = '';
+  value = '';
+  const row = listTable.insertRow(0);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const button = homeworkContainer.createElement('BUTTON');
+  button.innerHTML = 'Удалить';
+  cell1.innertext = name;
+  cell2.innertext = value;
+  cell3.innerHTML = button.innerHTML;
+});
+
+button.addEventListener('click', () => {
+  listTable.deleteRow(name);
+});
+
+listTable.addEventListener('click', (e) => {});
