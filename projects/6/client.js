a26256809788e4968aadb32a6728f13ff4cd9cd2
Natalia Myshkina <natycik9494@mail.ru>
2020-10-27 19:44:27 -0400

    final

--- projects/6/client.js
+++ projects/6/client.js
@@ -1,11 +1,13 @@
-//const { doc } = require("prettier");
 const ws = new WebSocket('ws://localhost:8580');
 const btn = document.getElementById('sendForm');
 const sendMessage = document.getElementById('but');
-const changePhoto = document.getElementById('photoChange');
+const online = [];
+const basa = [];
+const img = document.getElementById('photo');
 function show(div) {
   document.getElementById(div).classList.remove('hidden');
 }
+
 function hide(div) {
   document.getElementById(div).classList.add('hidden');
 }
@@ -13,40 +15,81 @@ function hide(div) {
 function user(name) {
   document.querySelector('#name').innerText = name;
 }
-
-
+//кнопка - отправить сообщение
 sendMessage.addEventListener('click', function () {
-  let text = document.getElementById('mes').value;
+  const text = document.getElementById('mes').value;
   const name = document.getElementById('name');
   const img = document.getElementById('photo').src;
   const request = {
     type: 'text',
     message: text,
-    name: name.innerText, 
-    image: img
-  }
+    name: name.innerText,
+    image: img,
+  };
+  if (text.length !== 0){
   ws.send(JSON.stringify(request));
-  document.getElementById('mes').value = '';
+  document.getElementById('mes').value = '';}
 });
+//кнопка - ввойти в чат
 btn.addEventListener('click', function () {
   const userName = document.getElementById('loginText').value;
-  const img = document.getElementById('photo').src = "./no.gif"; 
-  const request = {
-    type: 'login',
-    id: userName,
-    image: img
-  };
-
+  let img = (document.getElementById('photo').src = './no.gif');
   if (!userName) {
     alert('Введите Никнейм');
-  } else {
-    ws.send(JSON.stringify(request));
-    hide('log');
-    show('Chat');
-    user(userName);
+  } else {  
+    //если аккаунт уже зарегестрирован   
+    if (JSON.stringify(basa).includes(userName)) {
+      const photo = [];
+      for (const b of basa){
+        if(b.userName === userName){
+          photo.push(b.image);
+        }
+      }
+     //если аккаунт уже был в этой сессии онлайн
+      if (online.length !== 0 && JSON.stringify(online).includes(userName)) {
+        for (const u of online) {
+          if (u.status === 'online' && u.id === userName) {
+            alert('Данный user уже онлайн!');
+            document.getElementById('loginText').value = '';
+          } else if (u.status === 'offline' && u.id === userName) {
+            img = document.getElementById('photo').src = photo.toString();
+            const request = {
+              type: 'login',
+              id: userName,
+              image: img,
+            };
+            ws.send(JSON.stringify(request));
+            hide('log');
+            show('Chat');
+            user(userName);
+          } 
+        }
+      } else {  
+        img = document.getElementById('photo').src = photo.toString();
+        const request = {
+          type: 'login',
+          id: userName,
+          image: img,
+        };
+        ws.send(JSON.stringify(request));
+        hide('log');
+        show('Chat');
+        user(userName);
+      }
+    } else {
+      const request = {
+        type: 'login',
+        id: userName,
+        image: img,
+      };
+      ws.send(JSON.stringify(request));
+      hide('log');
+      show('Chat');
+      user(userName);
+    }
   }
 });
-const img = document.getElementById('photo')
+//Изменить фото аккаунта с помощью drag and drop
 img.addEventListener('dragover', (e) => {
   if (e.dataTransfer.items.length && e.dataTransfer.items[0].kind === 'file') {
     e.preventDefault();
@@ -54,61 +97,99 @@ img.addEventListener('dragover', (e) => {
 });
 img.addEventListener('drop', (e) => {
   const file = e.dataTransfer.files[0];
-  img.src = "./"+ file.name;
+  img.src = './' + file.name;
   e.preventDefault();
   const name = document.getElementById('name');
   const imge = document.getElementById('photo').src;
   const request = {
     type: 'change',
     name: name.innerText,
-    imge: imge
-  }
+    imge: imge,
+  };
   ws.send(JSON.stringify(request));
 });
-
+// когда юзер обновляет/close страницу - он выходит
+window.onunload = function () {
+  const n = document.getElementById('name');
+  const request = {
+    type: 'close',
+    message: ' вышел из чата',
+    name: n.innerText,
+  };
+  ws.send(JSON.stringify(request));
+};
+//принимаем с сервера
 ws.onmessage = function (event) {
   const message = JSON.parse(event.data);
-
   switch (message.type) {
-    case 'login':
-
-      const t = document.createElement('div');
-      t.innerText = message.responseBody + ' вошел в чат';
-      document.querySelector('.mContainer').appendChild(t);
-      const d = document.getElementById('us');
-      d.innerHTML = '';
-      for (var key of message.clients) {
-        if (key.status === 'online') {
-          const u = document.createElement('div');
-          u.innerText = key.id;
-          document.querySelector('#us').appendChild(u);
-        }
-      };
+    case 'connection':
+      for (const k of message.basa) {
+        basa.push(k);
+      }
+      for (const o of message.clients) {
+        online.push(o);
+      }
       break;
+
+    case 'login':
+      const n = document.querySelector('#name');
+      console.log(message.clients);
+      if (message.clients.length !== 0 && n.innerText.length !== 0) {
+        if (message.responseBody !== n.innerText) {
+          const t = document.createElement('div');
+          t.innerText = message.responseBody + ' вошел в чат';
+          document.querySelector('.mContainer').appendChild(t);
+        }
+        const d = document.getElementById('us');
+        d.innerHTML = '';
+        for (const key of message.clients) {
+          if (key.status === 'online') {
+            const u = document.createElement('div');
+            u.innerText = key.id;
+            document.querySelector('#us').appendChild(u);
+          }
+        }
+      }
+      break;
+
     default:
       console.error('Unknown RequestType');
       break;
+
     case 'text':
       document.getElementsByClassName('mContainer');
       const m = document.createElement('div');
-      m.innerHTML = 
-      `<div class="s">
-          <img id="${message.name}" class="img" src='${message.image}'>
+      m.innerHTML = `<div class="s">
+          <img class="${message.name}" src='${message.image}'>
             <span>${message.name}</span>
         <div>${message.responseBody}</div>
       </div>`; 
       document.querySelector('.mContainer').appendChild(m);
+      document.getElementById('cont').scrollTop = document.getElementById('cont').scrollHeight;
       break;
+
     case 'close':
       document.getElementsByClassName('mContainer');
       const c = document.createElement('div');
       c.innerText = message.responseBody;
       document.querySelector('.mContainer').appendChild(c);
+      document.getElementById('us').innerHTML = '';
+      for (const key of message.clients) {
+        if (key.status === 'online') {
+          const u = document.createElement('div');
+          u.innerText = key.id;
+          document.querySelector('#us').appendChild(u);
+        }
+      }
       break;
+      
     case 'change':
       const mesCon = document.getElementsByClassName('mContainer');
-      let b = message.name;
-      document.getElementById(b).src = message.image;
+      const b = message.name;
+      if(document.getElementsByClassName(b) !== null){
+        var el = document.getElementsByClassName(b);
+        for (var i =0; i<el.length; i++){
+      el[i].src = message.image;}}
       break;
   }
 };
@@ -124,11 +205,4 @@ ws.onopen = function () {
 
 ws.onclose = function () {
   console.log('Server Die');
-  const n = document.getElementById('name');
-  const request = {
-    type: 'close',
-    message: ' Вышел из чата',
-    name: n.innerText,
-  };
-  ws.send(JSON.stringify(request));
 };
--- projects/6/server.js
+++ projects/6/server.js
@@ -4,73 +4,99 @@ const clients = [];
 webSocketConfig.connections = webSocketConfig.clients;
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const uuidv1 = require('uuid/v1');
+//const uuidv1 = require('uuid/v1');
 
-const adapter = new FileSync('db.json');
-const db = low(adapter);
+const adapter = new FileSync('dbb.json');
+const dbb = low(adapter);
 
 webSocketConfig.on('connection', function (wsParams) {
-  wsParams.on('message', function (data, all = false) {
+  //загрузка данных с базы данных
+  const basa = dbb.get('users').value();
+  const response = {
+    type: 'connection',
+    basa: basa,
+    clients: clients,
+  };
+  wsParams.send(JSON.stringify(response));
+
+  wsParams.on('message', function (data) {
     const message = JSON.parse(data);
-    if (message.id) {clients.push({
-      id: message.id,
-      status: 'online',
-      image: message.image
-    })};
+    //список аккаунтов, что были/есть онлайн
+    if (!JSON.stringify(clients).includes(message.id) && message.id) {
+      clients.push({
+        id: message.id,
+        status: 'online',
+        image: message.image,
+      });
+    };
 
     webSocketConfig.connections.forEach((wsParams) => {
       switch (message.type) {
         case 'login':
-          wsParams.id = message.id;
-          const id = uuidv1();
           const response = {
             type: 'login',
             responseBody: message.id,
-            id: id,
             clients: clients,
             status: 'online',
-            image: message.image
+            image: message.image,
           }
-        
+          // добавляет статус онлайн
+          for (const key of clients) {
+            if (key.id === message.id) {
+              key.status = 'online';
+            }
+          }
           wsParams.send(JSON.stringify(response));
-          if (!db.hasIn(message.id)) {   
-            db.get('users').push({id: id, userName: message.id }).write();
-          }; 
-        break;
-
+          //если юзер новый, добавляем его в базу данных
+          if (dbb.get('users').find({ userName: message.id }).value() == null) {
+            dbb.get('users').push({ userName: message.id, image: message.image }).write();
+          }
+          break;
+//отправка сообщения
         case 'text':
           const mes = {
             type: 'text',
             responseBody: message.message,
             name: message.name,
-            image: message.image
+            image: message.image,
           };
           wsParams.send(JSON.stringify(mes));
           break;
-
+//выход юзера из сети
         case 'close':
           const close = {
             type: 'close',
-            responseBody: message.name + message.message
+            responseBody: message.name + message.message,
+            clients: clients,
           };
-           
+          for (const key of clients) {
+            if (key.id === message.name) {
+              key.status = 'offline';
+            }
+          }
           wsParams.send(JSON.stringify(close));
           break;
-
+//загрузка фотографии в профиль
         case 'change':
           const change = {
             type: 'change',
             name: message.name,
             image: message.imge,
-            client: clients
-          }
-          for (var key of clients) {
-            if (key.id === message.name){
+            client: clients,
+          };
+          //обновляем фото в базе данных
+          dbb
+            .get('users')
+            .find({ userName: message.name })
+            .assign({ image: message.imge })
+            .write();
+          for (const key of clients) {
+            if (key.id === message.name) {
               key.image = message.imge;
             }
           }
           wsParams.send(JSON.stringify(change));
-          
+
           break;
       }
     });
